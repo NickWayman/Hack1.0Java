cd72447f21684fe24488efb7675fec86c997c8d8
Nicho <Nicho@NWLaptop.unl.edu>
2022-08-27 20:49:39 -0500

    Added my minors

--- src/Hello.java
+++ src/Hello.java
@@ -3,5 +3,6 @@ public class Hello {
 	public static void main(String args[]) {
 		System.out.println("Nicholas Wayman");
 		System.out.println("My major is Chemical Engineering!");
+		System.out.println("My minors are in physics and computer science");
 	}
 }
